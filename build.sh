047b0ae87a1543f91477778cdd7e017d965dfbe8
Richard Snider <rsnider@skyslope.com>
2019-09-17 22:49:06 +0000

    fix build script logging

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 timestamp="$(date +"%Y-%m-%d-T-%H-%M-%S")"
 outputFileName="build-{$timestamp}.log"
 
-docker build -t ubuntu-xfce-vnc . >> outputFileName
+docker build -t ubuntu-xfce-vnc . >> $outputFileName
