540f5e4d1dc8c2c165c9af2911d16c54a40320b3
Richard Snider <rsnider@skyslope.com>
2019-10-10 20:52:54 +0000

    rm ssh keygen

--- build/install-scripts/git.sh
+++ build/install-scripts/git.sh
@@ -2,7 +2,3 @@
 set -e
 
 apt-get -y install git
-
-# Silently generate an rsa 4k key with no password and save as id_rsa in .ssh directory
-mkdir $HOME/.ssh
-ssh-keygen -t rsa -b 4096 -q -N "" -f $HOME/.ssh/id_rsa
