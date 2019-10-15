3c15ac27033ecf67487968abe7a60673f546e3f0
Richard Snider <rsnider@skyslope.com>
2019-10-15 13:58:21 +0000

    Rewrite run command

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
-  
-export RANDOM_PASSWORD=$(openssl rand -hex 10) && \
-docker run --name vnc --detach --shm-size=256m -p 5901:5901 -p 6901:6901 -e VNC_RESOLUTION=1920x1080 -e VNC_PW=$RANDOM_PASSWORD ubuntu-xfce-vnc && \
-echo "Generated password is: $RANDOM_PASSWORD" && \
-unset RANDOM_PASSWORD
+
+RANDOM_PASSWORD=$(openssl rand -hex 10)
+echo "Generated password is: $RANDOM_PASSWORD"
+
+docker run \
+--name vnc \
+--detach \
+--shm-size=256m \
+-p 5901:5901 -p 6901:6901 \
+-e VNC_PW=$RANDOM_PASSWORD \
+ubuntu-xfce-vnc
