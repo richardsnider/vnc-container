6b7bb182ac5ea3700c78b5c3e57eea4b1b5b7501
Richard Snider <rsnider@skyslope.com>
2019-10-15 13:01:18 +0000

    Add git email build arg

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -eo pipefail
 timestamp="$(date +"%Y-%m-%d-T-%H-%M-%S")"
 outputFileName="build-$timestamp.log"
 
-docker build -t ubuntu-xfce-vnc . | tee $outputFileName
+docker build --build-arg GIT_EMAIL_ARG=${1:-"email@example.com"} -t ubuntu-xfce-vnc . | tee $outputFileName
