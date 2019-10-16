15ede4a8c8e4d4a6ee5168d53f5216095e070c3a
Richard Snider <rsnider@skyslope.com>
2019-10-16 15:15:52 +0000

    tweak ssh setup

--- build/setup-scripts/ssh_setup.sh
+++ build/setup-scripts/ssh_setup.sh
@@ -3,18 +3,15 @@ set -e
 
 mkdir $HOME/.ssh
 ssh-keygen -t rsa -b 4096 -q -N "" -C $GIT_EMAIL -f $HOME/.ssh/id_rsa
-cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
 
-echo "Host github.com" >> ~/.ssh/config
-echo "User git" >> ~/.ssh/config
-echo "Hostname github.com" >> ~/.ssh/config
-echo "PreferredAuthentications publickey" >> ~/.ssh/config
-echo "IdentityFile $HOME/.ssh/id_rsa" >> ~/.ssh/config
-
-chmod 400 ~/.ssh
+echo "Host github.com" >> $HOME/.ssh/config
+echo "User git" >> $HOME/.ssh/config
+echo "Hostname github.com" >> $HOME/.ssh/config
+echo "PreferredAuthentications publickey" >> $HOME/.ssh/config
+echo "IdentityFile $HOME/.ssh/id_rsa" >> $HOME/.ssh/config
 
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
+ssh-add $HOME/.ssh/id_rsa
 
-# so this works . . . . .
-# ssh -i ~/.ssh/id_rsa -Tv git@github.com
+chmod 700 $HOME/.ssh
