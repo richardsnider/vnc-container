ca65837340300a455e26474e38d35818181f4fa7
Richard Snider <rsnider@skyslope.com>
2019-12-18 21:22:00 +0000

    improve build script

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-timestamp="$(date +"%Y-%m-%d-T-%H-%M-%S")"
-outputFileName="build-$timestamp.log"
+word1=$(grep -E -e '^[a-z]{4,4}$' /usr/share/dict/words | sort --random-sort | sed -e '$!d')
+word2=$(grep -E -e '^[a-z]{4,4}$' /usr/share/dict/words | sort --random-sort | sed -e '$!d')
+tag="${word1}_${word2}"
 
-docker build -t ubuntu-xfce-vnc . | tee $outputFileName
+outputFileName="${tag}_build.log"
+timestamp="$(date +"%Y-%m-%d-T-%H-%M-%S")"
+echo "Timestamp: $timestamp" | tee $outputFileName
+
+docker build --no-cache -t perennial:latest -t perennial:$tag . | tee $outputFileName
