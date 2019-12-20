f63a0547be644119837b7e0cb36484c36758a470
Richard Snider <rsnider@skyslope.com>
2019-12-20 00:28:38 +0000

    change build log file organization

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
+mkdir -p logs
+
 word1=$(grep -E -e '^[a-z]{4,4}$' /usr/share/dict/words | sort --random-sort | sed -e '$!d')
 word2=$(grep -E -e '^[a-z]{4,4}$' /usr/share/dict/words | sort --random-sort | sed -e '$!d')
 tag="${word1}_${word2}"
 
-outputFileName="${tag}_build.log"
+outputFileName="./logs/${tag}_build.log"
 timestamp="$(date +"%Y-%m-%d-T-%H-%M-%S")"
 echo "Timestamp: $timestamp" | tee $outputFileName
 
