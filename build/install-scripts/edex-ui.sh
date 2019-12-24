9be1a3c197526133f7fd16b725a22d69af22f5de
Richard Snider <rsnider@skyslope.com>
2019-12-24 18:14:30 +0000

    add -L to edex-ui curl

--- build/install-scripts/edex-ui.sh
+++ build/install-scripts/edex-ui.sh
@@ -5,7 +5,7 @@ echo "Downloading release for eDEX-UI (https://github.com/GitSquared/edex-ui) . 
 EDEX_DOWNLOAD_URL=$(curl https://api.github.com/repos/GitSquared/edex-ui/releases/latest | jq -r '.assets[].browser_download_url' | grep "eDEX-UI.Linux.x86_64.AppImage")
 echo "EDEX UI Download URL: $EDEX_DOWNLOAD_URL"
 
-curl $EDEX_DOWNLOAD_URL --output eDEX-UI.Linux.x86_64.AppImage
+curl -L $EDEX_DOWNLOAD_URL --output eDEX-UI.Linux.x86_64.AppImage
 chmod +x eDEX-UI.Linux.x86_64.AppImage
 
 ./eDEX-UI.Linux.x86_64.AppImage --appimage-extract
