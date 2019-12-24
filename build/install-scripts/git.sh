c73cbf3f72ffb4cc16379ce7ae0c319feacd13c3
Richard Snider <rsnider@skyslope.com>
2019-12-24 15:59:23 +0000

    move ssh setup into git

--- build/install-scripts/git.sh
+++ build/install-scripts/git.sh
@@ -2,3 +2,22 @@
 set -e
 
 apt-get -y install git
+
+mkdir $HOME/git
+chown -R 1000 $HOME/git
+
+mkdir $HOME/.ssh
+ssh-keygen -t rsa -b 4096 -q -N "" -f $HOME/.ssh/id_rsa
+cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+
+echo "Host github.com" >> $HOME/.ssh/config
+echo "User git" >> $HOME/.ssh/config
+echo "Hostname github.com" >> $HOME/.ssh/config
+echo "PreferredAuthentications publickey" >> $HOME/.ssh/config
+echo "IdentityFile $HOME/.ssh/id_rsa" >> $HOME/.ssh/config
+
+eval "$(ssh-agent -s)"
+ssh-add $HOME/.ssh/id_rsa
+
+chmod 700 $HOME/.ssh
+chown -R 1000 $HOME/.ssh
