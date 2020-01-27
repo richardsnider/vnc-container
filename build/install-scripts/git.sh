b0a5d0a615197bf080f6ce26343ddb280f10530a
Richard Snider <rsnider@skyslope.com>
2020-01-27 21:44:39 +0000

    remove redundant git setup

--- build/install-scripts/git.sh
+++ build/install-scripts/git.sh
@@ -2,22 +2,3 @@
 set -e
 
 apt-get -y install git
-
-mkdir $HOME/git
-chown --recursive 1000 $HOME/git
-
-mkdir $HOME/.ssh
-ssh-keygen -t rsa -b 4096 -q -N "" -f $HOME/.ssh/id_rsa
-cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
-
-echo "Host github.com" >> $HOME/.ssh/config
-echo "User git" >> $HOME/.ssh/config
-echo "Hostname github.com" >> $HOME/.ssh/config
-echo "PreferredAuthentications publickey" >> $HOME/.ssh/config
-echo "IdentityFile $HOME/.ssh/id_rsa" >> $HOME/.ssh/config
-
-eval "$(ssh-agent -s)"
-ssh-add $HOME/.ssh/id_rsa
-
-chmod --recursive 700 $HOME/.ssh
-chown --recursive 1000 $HOME/.ssh
