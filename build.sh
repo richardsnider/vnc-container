83e23091a2953fec6f09847ed6e8ff46c71a5406
Richard Snider <rsnider@skyslope.com>
2020-01-27 20:45:54 +0000

    add no-cache clean build option

--- build.sh
+++ build.sh
@@ -11,4 +11,9 @@ outputFileName="./logs/${tag}_build.log"
 timestamp="$(date +"%Y-%m-%d-T-%H-%M-%S")"
 echo "Timestamp: $timestamp" | tee $outputFileName
 
-docker build --no-cache -t perennial:latest -t perennial:$tag . | tee $outputFileName
+echo "Rebuild from scratch (--no-cache)? (y/n)"
+read $REBUILD_PERENNIAL_FROM_SCRATCH
+if [ "$REBUILD_PERENNIAL_FROM_SCRATCH" = "y" || "$REBUILD_PERENNIAL_FROM_SCRATCH" = "yes" || "$REBUILD_PERENNIAL_FROM_SCRATCH" = "Y"]
+then docker build --no-cache -t perennial:latest -t perennial:$tag . | tee $outputFileName
+else docker build -t perennial:latest -t perennial:$tag . | tee $outputFileName
+fi
