da3f7d374025de44c07a3ada1e0df778a2de7944
Richard Snider <rsnider@skyslope.com>
2020-03-30 17:03:58 +0100

    dark xfce theme

--- build/setup/scripts/xfce_ui.sh
+++ build/setup/scripts/xfce_ui.sh
@@ -2,5 +2,11 @@
 set -e
 
 echo "Install Xfce4 UI components"
-apt-get install -y supervisor xfce4 xfce4-terminal xterm
+apt-get install -y supervisor xfce4 xfce4-terminal xterm wmctrl
 apt-get purge -y pm-utils xscreensaver*
+
+mv $BUILD_DIRECTORY/setup/content/dark-theme /usr/share/themes/dark-theme
+
+# run window in the background
+# wmctrl -r :ACTIVE: -b remove,fullscreen
+# wmctrl -r :ACTIVE: -b add,skip_taskbar
