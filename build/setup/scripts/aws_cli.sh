1c08de5f1ec5a5031b6eba276ca8f9e4c0b20b69
Richard <richardnsnider@gmail.com>
2020-04-06 13:46:13 -0700

    aws cli install cleanup

--- build/setup/scripts/aws_cli.sh
+++ build/setup/scripts/aws_cli.sh
@@ -2,8 +2,9 @@
 set -ex
 
 echo "As directed by https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html"
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "$HOME/awscliv2.zip"
+unzip $HOME/awscliv2.zip
+sudo $HOME/aws/install
+rm -rf $HOME/aws $HOME/awscliv2.zip
 
 aws --version
